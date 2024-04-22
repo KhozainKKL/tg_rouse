f86f64412a865e05c973a539f93b5b8d6560cd78
artiom_naumenko_1 <anaumenko484@bk.ru>
2024-04-22 12:42:37 +0300

    create index.html

--- config/bot/main_bot.py
+++ config/bot/main_bot.py
@@ -1,6 +1,7 @@
 import telebot
 from django.conf import settings
 from telebot.async_telebot import AsyncTeleBot
+from telebot.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 
 bot = AsyncTeleBot(settings.TG_API_KEY, parse_mode='HTML')
 telebot.logger.setLevel(settings.LOGLEVEL)
@@ -8,4 +9,6 @@ telebot.logger.setLevel(settings.LOGLEVEL)
 
 @bot.message_handler(commands=['start'])
 async def echo_mes(message):
-    await bot.send_message(chat_id=message.chat.id, text='Привет.')
+    markup = ReplyKeyboardMarkup()
+    markup.add(KeyboardButton('Открыть веб страницу', web_app=WebAppInfo('https://127.0.0.1:8000/admin')))
+    await bot.send_message(chat_id=message.chat.id, text='Привет.', reply_markup=markup)
