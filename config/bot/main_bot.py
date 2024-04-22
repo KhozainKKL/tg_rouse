3d210f192cde33daa7ace858b585f26cfeb9e367
artiom_naumenko_1 <anaumenko484@bk.ru>
2024-04-22 13:21:50 +0300

    change index.html

--- config/bot/main_bot.py
+++ config/bot/main_bot.py
@@ -10,5 +10,7 @@ telebot.logger.setLevel(settings.LOGLEVEL)
 @bot.message_handler(commands=['start'])
 async def echo_mes(message):
     markup = ReplyKeyboardMarkup()
-    markup.add(KeyboardButton('Открыть веб страницу', web_app=WebAppInfo('https://127.0.0.1:8000/admin')))
+    markup.add(KeyboardButton('Открыть веб страницу', web_app=WebAppInfo(
+        url='https://htmlpreview.github.io/'
+            '?https://github.com/KhozainKKL/tg_rouse/blob/main/config/templates/index.html')))
     await bot.send_message(chat_id=message.chat.id, text='Привет.', reply_markup=markup)
