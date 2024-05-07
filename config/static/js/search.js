142f5c46b8d3984dc5ac34b9ed1182d7318f6b57
artiom_naumenko_1 <anaumenko484@bk.ru>
2024-05-07 12:07:53 +0300

    fix templates

--- config/static/js/search.js
+++ config/static/js/search.js
@@ -1,50 +1,53 @@
-let tg = window.Telegram.WebApp;
-let search = document.getElementById("search");
-tg.expand();
-// Получаем параметры запроса URL
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
+document.addEventListener('DOMContentLoaded', function() {
+    let tg = window.Telegram.WebApp;
+    let search = document.getElementById("search");
+    tg.expand();
+    // Получаем параметры запроса URL
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
 
-// Получаем ссылки на элементы <select>
-const sortParams = urlParams.get('sort');
-const decodedSortParams = decodeURIComponent(sortParams);
-const sortOptions = JSON.parse(decodedSortParams);
+    // Получаем ссылки на элементы <select>
+    const sortParams = urlParams.get('sort');
+    const decodedSortParams = decodeURIComponent(sortParams);
+    const sortOptions = JSON.parse(decodedSortParams);
 
-const categoryParams = urlParams.get('category');
-const decodedCategoryParams = decodeURIComponent(categoryParams);
-const categoryOptions = JSON.parse(decodedCategoryParams);
+    const categoryParams = urlParams.get('category');
+    const decodedCategoryParams = decodeURIComponent(categoryParams);
+    const categoryOptions = JSON.parse(decodedCategoryParams);
 
-// Заполняем список опций для элемента <select> с id="sort"
-sortOptions.forEach(option => {
-    const optionElement = document.createElement('option');
-    optionElement.value = option;
-    optionElement.textContent = option;
-    sortSelect.appendChild(optionElement);
+    // Заполняем список опций для элемента <select> с id="sort"
+    const sortSelect = document.getElementById('sort');
+    sortOptions.forEach(option => {
+        const optionElement = document.createElement('option');
+        optionElement.value = option;
+        optionElement.textContent = option;
+        sortSelect.appendChild(optionElement);
+    });
+
+    // Заполняем список опций для элемента <select> с id="category"
+    const categorySelect = document.getElementById('category');
+    categoryOptions.forEach(option => {
+        const optionElement = document.createElement('option');
+        optionElement.value = option;
+        optionElement.textContent = option;
+        categorySelect.appendChild(optionElement);
+    });
+
+    search.addEventListener("click", () => {
+        var sortValue = document.getElementById("sort").value;
+        var categoryValue = document.getElementById("category").value;
+        var colorValue = document.getElementById("color").value;
+
+        var data = {
+            sort: sortValue,
+            category: categoryValue,
+            color: colorValue
+        };
+        //console.log(data);
+        //event.preventDefault();
+
+
+        tg.sendData(JSON.stringify(data));
+        tg.close();
+    });
 });
-
-// Заполняем список опций для элемента <select> с id="category"
-categoryOptions.forEach(option => {
-    const optionElement = document.createElement('option');
-    optionElement.value = option;
-    optionElement.textContent = option;
-    categorySelect.appendChild(optionElement);
-});
-
-
- search.addEventListener("click", () => {
-    var sortValue = document.getElementById("sort").value;
-    var categoryValue = document.getElementById("category").value;
-    var colorValue = document.getElementById("color").value;
-
-    var data = {
-        sort: sortValue,
-        category: categoryValue,
-        color: colorValue
-    };
-//console.log(data);
-//event.preventDefault();
-
-
-    tg.sendData(JSON.stringify(data));
-    tg.close();
- });
