a0ec1514d98d52b786b410d90154b944b480a352
artiom_naumenko_1 <anaumenko484@bk.ru>
2024-05-16 11:53:54 +0300

    initial create api for products and users. initial create model for products create databasep[sqlite3]

---
+++ main.py
@@ -0,0 +1,23 @@
+from contextlib import asynccontextmanager
+
+import uvicorn
+from fastapi import FastAPI
+from core.models import Base, db_helper
+
+from api_v1 import router as router_v1
+
+from core.config import settings
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router_v1, prefix=settings.api_v1_prefix)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8080, reload=True)
