8b8afbba566433a856a91f6b169c5baee9ae4af6
artiom_naumenko_1 <anaumenko484@bk.ru>
2024-06-04 13:17:44 +0300

    fix bags

--- bot/run.py
+++ bot/run.py
@@ -53,97 +53,99 @@ async def start_message(message):
         await bot.send_message(chat_id=message.chat.id, text=message.web_app_data.data)
 
 
-@bot.message_handler(commands=["cart"])
-async def cart(message):
-    encoded_data = urllib.parse.quote(
-        json.dumps(
-            await db.get_fetch_data(url_template="cart", data_id=message.from_user.id)
-        )
-    )
-    url = BASE_URL + f"cart.html?product={encoded_data}"
-    markup = InlineKeyboardMarkup()
-    markup.add(InlineKeyboardButton("Корзина 🧺", web_app=WebAppInfo(url=url)))
-    await bot.send_message(chat_id=message.chat.id, text="Привет.", reply_markup=markup)
-
-
-@bot.message_handler(commands=["users", "order"])
-async def echo_message(message):
-    result = await db.get_fetch_data(message.text, message.from_user.id)
-    await bot.send_message(message.chat.id, result)
-
-
-@bot.message_handler(commands=["products"])
-async def echo_message(message):
-    result = await db.get_fetch_data(message.text)
-    await bot.send_message(message.chat.id, result)
-
-
-@bot.message_handler(commands=["search"])
-async def search_product(message):
-    products = await db.get_fetch_data(message.text)
-    encoded_sort = urllib.parse.quote(json.dumps(products))
-
-    url = BASE_URL + f"search.html?product={encoded_sort}"
-    markup = ReplyKeyboardMarkup()
-    markup.add(KeyboardButton("Поиск товара 🔎", web_app=WebAppInfo(url=url)))
-    await bot.send_message(chat_id=message.chat.id, text="Привет.", reply_markup=markup)
-
-    @bot.message_handler(content_types=["web_app_data"])
-    async def result_search_product(message):
-        prod = await db.get_search_product(data=json.loads(message.web_app_data.data))
-        if not prod:
-            await bot.send_message(chat_id=message.chat.id, text="Товар не найден.")
-        if prod:
-            keyb_list_search_prod = InlineKeyboardMarkup()
-            print(prod)
-            for item in prod:
-                keyb_list_search_prod.add(
-                    InlineKeyboardButton(
-                        text=f'{item["name"]}', callback_data=f'data_{item["name"]}'
-                    )
-                )
-
-            await bot.send_message(
-                chat_id=message.chat.id,
-                text="Найденные товары",
-                reply_markup=keyb_list_search_prod,
-            )
-
-            @bot.callback_query_handler(lambda query: query.data.startswith("data_"))
-            async def post_details_search_product(query):
-                date = query.data.split("_")[1]
-                keyb_details_product = InlineKeyboardMarkup(row_width=1)
-                keyb_details_product.add(
-                    InlineKeyboardButton("Добавить в корзину 🧺", callback_data="2"),
-                    InlineKeyboardButton(
-                        "Назад", callback_data="back_to_list_search_product"
-                    ),
-                )
-                for items in prod:
-                    if items["name"] == date:
-                        text = (
-                            f'Название: {items["name"]}\n'
-                            f'Описание: {items["description"]}\n'
-                            f'Цена: {items["price"]}\n'
-                            f'<a href="https://khozainkkl.github.io/tg_rouse.github.io/tg_rouse/media/{items["image"]}"> </a>'
-                        )
-                await bot.edit_message_text(
-                    chat_id=query.message.chat.id,
-                    text=text,
-                    message_id=query.message.message_id,
-                    reply_markup=keyb_details_product,
-                )
-
-                @bot.callback_query_handler(
-                    func=lambda call: call.data == "back_to_list_search_product"
-                )
-                async def back_to_list_search_product(call):
-                    await bot.edit_message_text(
-                        chat_id=call.message.chat.id,
-                        text="Найденные товары",
-                        message_id=call.message.message_id,
-                        reply_markup=keyb_list_search_prod,
-                    )
+#
+# @bot.message_handler(commands=["cart"])
+# async def cart(message):
+#     encoded_data = urllib.parse.quote(
+#         json.dumps(
+#             await db.get_fetch_data(url_template="cart", data_id=message.from_user.id)
+#         )
+#     )
+#     url = BASE_URL + f"cart.html?product={encoded_data}"
+#     markup = InlineKeyboardMarkup()
+#     markup.add(InlineKeyboardButton("Корзина 🧺", web_app=WebAppInfo(url=url)))
+#     await bot.send_message(chat_id=message.chat.id, text="Привет.", reply_markup=markup)
+#
+#
+# @bot.message_handler(commands=["users", "order"])
+# async def echo_message(message):
+#     result = await db.get_fetch_data(message.text, message.from_user.id)
+#     await bot.send_message(message.chat.id, result)
+#
+#
+# @bot.message_handler(commands=["products"])
+# async def echo_message(message):
+#     result = await db.get_fetch_data(message.text)
+#     await bot.send_message(message.chat.id, result)
+#
+#
+# @bot.message_handler(commands=["search"])
+# async def search_product(message):
+#     products = await db.get_fetch_data(message.text)
+#     encoded_sort = urllib.parse.quote(json.dumps(products))
+#
+#     url = BASE_URL + f"search.html?product={encoded_sort}"
+#     markup = ReplyKeyboardMarkup()
+#     markup.add(KeyboardButton("Поиск товара 🔎", web_app=WebAppInfo(url=url)))
+#     await bot.send_message(chat_id=message.chat.id, text="Привет.", reply_markup=markup)
+#
+#     @bot.message_handler(content_types=["web_app_data"])
+#     async def result_search_product(message):
+#         prod = await db.get_search_product(data=json.loads(message.web_app_data.data))
+#         if not prod:
+#             await bot.send_message(chat_id=message.chat.id, text="Товар не найден.")
+#         if prod:
+#             keyb_list_search_prod = InlineKeyboardMarkup()
+#             print(prod)
+#             for item in prod:
+#                 keyb_list_search_prod.add(
+#                     InlineKeyboardButton(
+#                         text=f'{item["name"]}', callback_data=f'data_{item["name"]}'
+#                     )
+#                 )
+#
+#             await bot.send_message(
+#                 chat_id=message.chat.id,
+#                 text="Найденные товары",
+#                 reply_markup=keyb_list_search_prod,
+#             )
+#
+#             @bot.callback_query_handler(lambda query: query.data.startswith("data_"))
+#             async def post_details_search_product(query):
+#                 date = query.data.split("_")[1]
+#                 keyb_details_product = InlineKeyboardMarkup(row_width=1)
+#                 keyb_details_product.add(
+#                     InlineKeyboardButton("Добавить в корзину 🧺", callback_data="2"),
+#                     InlineKeyboardButton(
+#                         "Назад", callback_data="back_to_list_search_product"
+#                     ),
+#                 )
+#                 for items in prod:
+#                     if items["name"] == date:
+#                         text = (
+#                             f'Название: {items["name"]}\n'
+#                             f'Описание: {items["description"]}\n'
+#                             f'Цена: {items["price"]}\n'
+#                             f'<a href="https://khozainkkl.github.io/tg_rouse.github.io/tg_rouse/media/{items["image"]}"> </a>'
+#                         )
+#                 await bot.edit_message_text(
+#                     chat_id=query.message.chat.id,
+#                     text=text,
+#                     message_id=query.message.message_id,
+#                     reply_markup=keyb_details_product,
+#                 )
+#
+#                 @bot.callback_query_handler(
+#                     func=lambda call: call.data == "back_to_list_search_product"
+#                 )
+#                 async def back_to_list_search_product(call):
+#                     await bot.edit_message_text(
+#                         chat_id=call.message.chat.id,
+#                         text="Найденные товары",
+#                         message_id=call.message.message_id,
+#                         reply_markup=keyb_list_search_prod,
+#                     )
+#
 
 
 async def log_out_function():
